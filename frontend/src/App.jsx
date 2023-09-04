588824e22a0eb8f5525ce385e2ce2d58f3f16175
martin-creator <martinlubowa@outlook.com>
2023-09-04 22:43:25 +0300

    Configured react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 
-
 function App() {
   return (
     <>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,26 @@ import App from './App.jsx'
 //import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import HomeScreen from './screens/HomeScreen.jsx'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouteProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+        <Route index={true} path='/' element={<HomeScreen />} /> 
+    </Route>
+
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouteProvider router={router} />
   </React.StrictMode>,
 )
