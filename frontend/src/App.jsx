ae0c2a31d7af234d79526b9d0b4ded5a9703fca9
martin-creator <martinlubowa@outlook.com>
2023-09-04 20:40:39 +0300

    Fixed bootstrap import bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,17 @@
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-
-
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
 
 function App() {
-
-
-
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to Electro Shop</h1>
+          <h1>Welcome to Electro Shops</h1>
         </Container>
       </main>
-
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
