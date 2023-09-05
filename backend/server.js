70a29e3c32d3f2dd1d4b27cf6977ffb9d4a0d650
martin-creator <martinlubowa@outlook.com>
2023-09-05 23:22:38 +0300

    Added logic to return specific product

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import products from './data/products';
+import products from './data/products.js'
 
 const port = 5000;
 
@@ -12,6 +12,11 @@ app.get('/', (req, res) => {
 
 app.get('/api/products', (req, res) => {
     res.json(products);
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((x) => x._id === req.params.id);
+    res.json(product);
 })
 
 
