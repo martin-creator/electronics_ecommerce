3031d3e0e8514b5483d659ebef1aa1fddbcb1c6d
martin-creator <martinlubowa@outlook.com>
2023-09-06 23:53:10 +0300

    Fixed import bug

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import colors from "colors";
-import users from "./data/users";
-import products from "./data/products";
-import User from "./Models/userModel";
-import Product from "./Models/productModel";
-import Order from "./Models/orderModel";
-import connectDB from "./config/db";
+import users from "./data/users.js";
+import products from "./data/products.js";
+import User from "./Models/userModel.js";
+import Product from "./Models/productModel.js";
+import Order from "./Models/orderModel.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
