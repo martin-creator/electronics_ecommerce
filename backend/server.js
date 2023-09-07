81a94a4aa51a21de2d1160eba14f0542b0222a2e
martin-creator <martinlubowa@outlook.com>
2023-09-07 08:14:54 +0300

    Created products routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import products from './data/products.js';
 import dotenv from 'dotenv';
 import cors from 'cors'
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -19,14 +19,9 @@ app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((x) => x._id === req.params.id);
-    res.json(product);
-})
+
 
 
 app.listen(port, () => {
