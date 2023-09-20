dd17cd1c18fd4b65d8074dfc682637f26c0a36b8
martin-creator <martinlubowa@outlook.com>
2023-09-20 20:37:08 +0300

    Created JWT and stored on cookie on server

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../Models/userModel.js";
+import jwt from "jsonwebtoken";
 
 
 // @desc    Auth user & get token
@@ -13,6 +14,18 @@ const authUser = asyncHandler(async(req, res) => {
     const user = await User.findOne({email});
 
     if (user && (await user.matchPassword(password))) {
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {
+            expiresIn: '30d'});
+
+        // set JWT as cookie
+        res.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+            secure: process.env.NODE_ENV !== 'development',
+            sameSite: 'strict'
+        });
+
+
         res.json({
             _id: user._id,
             name: user.name,
