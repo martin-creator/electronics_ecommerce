d40118a8a9ee506e34b4a5f1c9ab9bcbc34aaac8
martin-creator <martinlubowa@outlook.com>
2023-09-20 18:48:16 +0300

    Added CRUD routes for user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,64 @@
 import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../Models/productModel.js";
+import User from "../Models/userModel.js";
 
 
-// @desc    Fetch all products
-// @route   GET /api/products
+// @desc    Auth user & get token
+// @route   GET /api/users/login
 // @access  Public
+
+const authUser = asyncHandler(async(req, res) => {
+    res.send('Login Route');
+});
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+
+const registerUser = asyncHandler(async(req, res) => {
+    res.send('Register Route');
+});
+
+// @desc    Logout user & clear cookie
+// @route   GET /api/users/logout
+// @access  Private
+
+const logoutUser = asyncHandler(async(req, res) => {
+    res.send('Logout Route');
+}
+);
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Public
+
+const getUserProfile = asyncHandler(async(req, res) => {
+    res.send('Profile Route');
+}
+);
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+
+const updateUserProfile = asyncHandler(async(req, res) => {
+    res.send('Update Profile Route');
+}
+);
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+
+const getUsers = asyncHandler(async(req, res) => {
+    res.send('Get Users Route');
+}
+);
+
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+
+const deleteUser = asyncHandler(async(req, res) => {
+    res.send('Delete User Route');
+}
+);
