d195eb15f9edcb5e3684b0f79b2acd7b78ee71b9
martin-creator <martinlubowa@outlook.com>
2023-09-20 21:53:23 +0300

    Created login middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import asyncHandler from './asyncHandler';
+import asyncHandler from './asyncHandler.js';
 import User from '../Models/userModel.js'
 
 // Protect routes
