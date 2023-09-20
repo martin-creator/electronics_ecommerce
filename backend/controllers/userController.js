5db82da74764d8730da0652b769d52ada3c8559a
martin-creator <martinlubowa@outlook.com>
2023-09-20 20:17:09 +0300

    Created basic auth backbone

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,7 +12,7 @@ const authUser = asyncHandler(async(req, res) => {
     // check if user exists
     const user = await User.findOne({email});
 
-    if (user) {
+    if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
             name: user.name,
