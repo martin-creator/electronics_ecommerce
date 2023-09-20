48db7c260a7f5b0f470ba80fb0248e0a23438596
martin-creator <martinlubowa@outlook.com>
2023-09-20 18:44:07 +0300

    Created user controller and routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,7 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../Models/productModel.js";
+
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
