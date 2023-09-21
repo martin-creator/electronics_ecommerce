43b5cd84758222ea78952285e93e00d3e15dd63b
martin-creator <martinlubowa@outlook.com>
2023-09-21 20:39:30 +0300

    created the order routes

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -109,3 +109,17 @@ const updateOrderToDelivered = asyncHandler(async(req, res) => {
     }
 }
 );
+
+
+// @desc    Get all orders
+// @route   GET /api/orders
+// @access  Private/Admin
+
+const getOrders = asyncHandler(async(req, res) => {
+    const orders = await Order.find({}).populate('user', 'id name');
+    res.json(orders);
+}
+);
+
+
+export {addOrderItems, getOrderById, updateOrderToPaid, getMyOrders, getOrders, updateOrderToDelivered};
