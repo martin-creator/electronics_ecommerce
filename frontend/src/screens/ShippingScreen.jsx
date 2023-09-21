0a0ce8023b83a27722b51b1eefda18a03c1a4ce8
martin-creator <martinlubowa@outlook.com>
2023-09-21 19:44:44 +0300

    Created payment screen

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import FormContainer from "../components/FormContainer"
 import { saveShippingAddress } from "../slices/cartSlice"
+import CheckoutSteps from "../components/CheckoutSteps"
 
 
 const ShippingScreen = () => {
@@ -29,6 +30,7 @@ const ShippingScreen = () => {
 
   return (  
     <FormContainer>
+        <CheckoutSteps step1 step2 />
         <h1>Shipping</h1>
         <Form onSubmit={submitHandler}>
             <Form.Group controlId="address" className="my-3">
