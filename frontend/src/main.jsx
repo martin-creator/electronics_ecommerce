5510ef4135f66addb0146ef5476e1511508aaf4b
martin-creator <martinlubowa@outlook.com>
2023-09-21 19:53:57 +0300

    created payment route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,6 +11,7 @@ import CartScreen from "./screens/CartScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
 import ShippingScreen from "./screens/ShippingScreen.jsx";
+import PaymentScreen from "./screens/PaymentScreen.jsx";
 
 import {
   createBrowserRouter,
@@ -34,6 +35,7 @@ const router = createBrowserRouter(
 
       <Route path='' element={<PrivateRoute />}>
         <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
       </Route>
     </Route>
   )
