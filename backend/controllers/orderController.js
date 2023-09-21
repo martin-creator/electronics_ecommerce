d28b565d6ac748930aedb4816309c4afe49af14e
martin-creator <martinlubowa@outlook.com>
2023-09-21 20:31:36 +0300

    Added routes to manage payments and delivers

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -40,3 +40,72 @@ const getMyOrders = asyncHandler(async(req, res) => {
     res.json(orders);
 }
 );
+
+
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+
+const getOrderById = asyncHandler(async(req, res) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+    if (order) {
+        res.json(order);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+}
+);
+
+
+// @desc    Update order to paid
+// @route   PUT /api/orders/:id/pay
+// @access  Private
+
+const updateOrderToPaid = asyncHandler(async(req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+        order.isPaid = true;
+        order.paidAt = Date.now();
+
+        // from PayPal
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            updateTime: req.body.update_time,
+            email_address: req.body.payer.email_address
+        };
+
+        const updatedOrder = await order.save();
+
+        res.json(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+}
+);
+
+
+// @desc    Update order to delivered
+// @route   PUT /api/orders/:id/deliver
+// @access  Private/Admin
+
+const updateOrderToDelivered = asyncHandler(async(req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+        order.isDelivered = true;
+        order.deliveredAt = Date.now();
+
+        const updatedOrder = await order.save();
+
+        res.json(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+}
+);
