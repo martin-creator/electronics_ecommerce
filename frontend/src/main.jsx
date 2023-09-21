356f5e9cf6922d63575e9612aa19ff84913ac545
martin-creator <martinlubowa@outlook.com>
2023-09-21 15:33:21 +0300

    Created shipping route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,7 @@ import ProductScreen from "./screens/ProductScreen.jsx";
 import CartScreen from "./screens/CartScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
+import ShippingScreen from "./screens/ShippingScreen.jsx";
 
 import {
   createBrowserRouter,
@@ -28,6 +29,7 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      <Route path="/shipping" element={<ShippingScreen />} />
     </Route>
   )
 );
