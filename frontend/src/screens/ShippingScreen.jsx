19b4b7f85d84cf252f62e269ef24e616a63243e4
martin-creator <martinlubowa@outlook.com>
2023-09-21 15:31:48 +0300

    created shipping screen

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import { useState } from "react"
+import { Form, Button } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
+import FormContainer from "../components/FormContainer"
+
 
 const ShippingScreen = () => {
+    const [address, setAddress] = useState("")
+    const [city, setCity] = useState("")
+    const [postalCode, setPostalCode] = useState("")
+    const [country, setCountry] = useState("")
   return (
     <div>ShippingScreen</div>
   )
