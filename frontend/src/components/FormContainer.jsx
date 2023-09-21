295a6d8a74eb421241a4ed3a969ab200bdd34f48
martin-creator <martinlubowa@outlook.com>
2023-09-21 13:24:19 +0300

    Created formcontainer element

---
+++ frontend/src/components/FormContainer.jsx
@@ -0,0 +1,20 @@
+import { Container, Row, Col } from 'react-bootstrap'
+import PropTypes from 'prop-types'
+
+const FormContainer = ({ children }) => {
+  return (
+    <Container>
+        <Row className='justify-content-md-center'>
+            <Col xs={12} md={6}>
+                {children}
+            </Col>
+        </Row>
+    </Container>
+  )
+}
+
+FormContainer.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+
+export default FormContainer
