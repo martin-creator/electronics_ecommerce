03a752df4d0416a982b10981b5af8e46bba93b1d
martin-creator <martinlubowa@outlook.com>
2023-09-21 19:52:49 +0300

    Created payment screen

--- frontend/src/screens/PaymentScreen.jsx
+++ frontend/src/screens/PaymentScreen.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import FormContainer from "../components/FormContainer"
+import { savePaymentMethod } from "../slices/cartSlice"
+import {Form, Button, Col} from "react-bootstrap"
+import CheckoutSteps from "../components/CheckoutSteps"
 
 const PaymentScreen = () => {
+    const [paymentMethod, setPaymentMethod] = useState("PayPal")
+
   return (
-    <div>PaymentScreen</div>
+    <FormContainer>
+        <CheckoutSteps step1 step2 step3 />
+        <h1>Payment Method</h1>
+        <Form onSubmit={submitHandler}>
+            <Form.Group>
+                <Form.Label as="legend">Select Method</Form.Label>
+                <Col>
+                    <Form.Check
+                        type="radio"
+                        label="PayPal or Credit Card"
+                        id="PayPal"
+                        name="paymentMethod"
+                        value="PayPal"
+                        checked
+                        onChange={(e) => setPaymentMethod(e.target.value)}
+                    ></Form.Check>
+                    <Form.Check
+                        type="radio"
+                        label="Stripe"
+                        id="Stripe"
+                        name="paymentMethod"
+                        value="Stripe"
+                        onChange={(e) => setPaymentMethod(e.target.value)}
+                    ></Form.Check>
+                </Col>
+            </Form.Group>
+            <Button type="submit" variant="primary">
+                Continue
+            </Button>
+        </Form>
+    </FormContainer>
   )
 }
 
