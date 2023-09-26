3cb2c948b97236f120ef8b8cab2dfbac1f18993f
martin-creator <martinlubowa@outlook.com>
2023-09-26 15:28:40 +0300

    Updated user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,176 +1,195 @@
-import asyncHandler from "../middleware/asyncHandler.js";
-import User from "../Models/userModel.js";
-import generateToken from "../utils/generateToken.js";
-
+import asyncHandler from '../middleware/asyncHandler.js';
+import generateToken from '../utils/generateToken.js';
+import User from '../models/userModel.js';
 
 // @desc    Auth user & get token
-// @route   GET /api/users/login
+// @route   POST /api/users/auth
 // @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-const authUser = asyncHandler(async(req, res) => {
-    const {email, password} = req.body;
+  const user = await User.findOne({ email });
 
-    // check if user exists
-    const user = await User.findOne({email});
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
 
-    if (user && (await user.matchPassword(password))) {
-        // generate token
-        generateToken(res, user._id);
-        res.status(200).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin
-        });
-    } else {
-        res.status(401);
-        throw new Error('Invalid email or password');
-    }
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
 
-const registerUser = asyncHandler(async(req, res) => {
-    const {name, email, password} = req.body;
+  const userExists = await User.findOne({ email });
 
-    // check if user exists
-    const userExits = await User.findOne({email});
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
 
-    if (userExits) {
-        res.status(400);
-        throw new Error('User already exists');
-    }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
 
-    // create user
-    const user = await User.create({
-        name,
-        email,
-        password
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
     });
-
-    if (user) {
-        // generate token
-        generateToken(res, user._id);
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin
-        });
-    } else {
-        res.status(400);
-        throw new Error('Invalid user data');
-    }
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
-// @desc    Logout user & clear cookie
-// @route   GET /api/users/logout
-// @access  Private
-
-const logoutUser = asyncHandler(async(req, res) => {
-    res.cookie('token', 'none', {
-        expires: new Date(Date.now(0)),
-        httpOnly: true
-    });
-
-    res.status(200).json({
-        message: 'User logged out successfully',
-        success: true,
-        data: {}
-    });
-}
-);
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/logout
+// @access  Public
+const logoutUser = (req, res) => {
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: 'Logged out successfully' });
+};
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
-// @access  Public
-
+// @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id);
 
-    if (user) {
-        res.status(200).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin
-        });
-    } else {
-        res.status(404);
-        throw new Error('User not found');
-    }
-}
-);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
 
-const updateUserProfile = asyncHandler(async(req, res) => {
-    const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
 
-    // check if user exists
-    if (user) {
-        user.name = req.body.name || user.name;
-        user.email = req.body.email || user.email;
-
-        // check if password is provided
-        if (req.body.password) {
-            user.password = req.body.password;
-        }
-
-        // save user
-        const updatedUser = await user.save();
-
-        res.status(200).json({
-            _id: updatedUser._id,
-            name: updatedUser.name,
-            email: updatedUser.email,
-            isAdmin: updatedUser.isAdmin
-        });
-    } else {
-        res.status(404);
-        throw new Error('User not found');
+    if (req.body.password) {
+      user.password = req.body.password;
     }
-}
-);
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
 
 // @desc    Get all users
 // @route   GET /api/users
 // @access  Private/Admin
-
-const getUsers = asyncHandler(async(req, res) => {
-    res.send('Get Users Route');
-}
-);
-
-// @desc    Get user by ID
-// @route   GET /api/users/:id
-// @access  Private/Admin
-
-const getUserById = asyncHandler(async(req, res) => {
-    res.send('Get User by ID Route');
-}
-);
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
 
 // @desc    Delete user
 // @route   DELETE /api/users/:id
 // @access  Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
 
-const deleteUser = asyncHandler(async(req, res) => {
-    res.send('Delete User Route');
-}
-);
+  if (user) {
+    if (user.isAdmin) {
+      res.status(400);
+      throw new Error('Can not delete admin user');
+    }
+    await User.deleteOne({ _id: user._id });
+    res.json({ message: 'User removed' });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
 
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
 // @desc    Update user
 // @route   PUT /api/users/:id
 // @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
 
-const updateUser = asyncHandler(async(req, res) => {
-    res.send('Update User Route');
-}
-);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
 
-export {authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, getUsers, getUserById, deleteUser, updateUser};
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+};
