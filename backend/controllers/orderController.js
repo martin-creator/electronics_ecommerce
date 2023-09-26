17de15b07d86154f7bdb456481ec62571d4aabbc
martin-creator <martinlubowa@outlook.com>
2023-09-26 15:11:24 +0300

    Cleaned orders controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,11 +1,10 @@
 import asyncHandler from '../middleware/asyncHandler.js';
-import Order from '../Models/orderModel.js';
+import Order from '../models/orderModel.js';
 
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
-   
   const {
     orderItems,
     shippingAddress,
@@ -39,8 +38,6 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
     res.status(201).json(createdOrder);
   }
-
- 
 });
 
 // @desc    Get logged in user orders
@@ -49,7 +46,6 @@ const addOrderItems = asyncHandler(async (req, res) => {
 const getMyOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find({ user: req.user._id });
   res.json(orders);
-  //res.send(JSON.stringify(req.user._id));
 });
 
 // @desc    Get order by ID
