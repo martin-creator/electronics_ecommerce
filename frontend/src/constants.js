e4e1a863f27a650e838de42a04d6fa0c3aa754c3
martin-creator <martinlubowa@outlook.com>
2023-10-05 21:20:26 +0300

    Updated constants

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,7 @@
 // Desc: Constants for the frontend
 //export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://mern-ecommerce-website.herokuapp.com' : ' http://localhost:5173'
-export const BASE_URL = 'http://localhost:3000'
-export const PRODUCTS_URL = `/api/products`
-export const USERS_URL = `/api/users`
-export const ORDERS_URL = `/api/orders`
-export const PAYPAL_URL = `/api/config/paypal`
+export const BASE_URL = ' ' // if you are using the proxy in vite.config.js
+export const PRODUCTS_URL = '/api/products'
+export const USERS_URL = '/api/users'
+export const ORDERS_URL = '/api/orders'
+export const PAYPAL_URL = '/api/config/paypal'
