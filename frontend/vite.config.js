2be1a5e0053c2774d2107c4608900dbfdffc050a
martin-creator <martinlubowa@outlook.com>
2023-10-05 21:28:55 +0300

    Updated vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/': 'http://localhost:3000/'
+      }
+    },
+    
   resolve: {
     alias: {
       '~bootstrap': ('./node_modules/bootstrap'),
