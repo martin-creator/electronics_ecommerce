9d4bb721cf82f5c4bf60481b485ee10508ce61ac
martin-creator <martinlubowa@outlook.com>
2023-10-05 21:21:00 +0300

    Uploaded loader.jsx

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,21 +1,18 @@
-import { Spinner } from "react-bootstrap";
+import { Spinner } from 'react-bootstrap';
 
 const Loader = () => {
   return (
     <Spinner
-        animation="border"
-        role="status"
-        style={{
-            width: "100px",
-            height: "100px",
-            margin: "auto",
-            display: "block",
-        }}
-    >
-        <span className="sr-only"></span>
-    </Spinner>
+      animation='border'
+      role='status'
+      style={{
+        width: '100px',
+        height: '100px',
+        margin: 'auto',
+        display: 'block',
+      }}
+    ></Spinner>
+  );
+};
 
-  )
-}
-
-export default Loader
+export default Loader;
